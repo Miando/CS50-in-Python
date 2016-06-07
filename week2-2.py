7bf0e1cbb6fa5d132a6d98e82211b03bb7de0914
miando <kuznecov513@gmail.com>
2016-06-07 13:08:47 +0300

    week2-2

--- week2-2.py
+++ week2-2.py
@@ -3,12 +3,18 @@ key=sys.argv[1]
 s=raw_input("input text: ")
 output=""
 for i in s:
-    if i.isalpha():
+    if i ==" ":
+        letter=" "
+    elif i.isalpha():
         in_alfabet= ord(i)+ int(key)
         if in_alfabet>ord("z") and i.islower()==True:
             in_alfabet=in_alfabet-26
         elif in_alfabet>ord("Z") and i.isupper()==True:
             in_alfabet=in_alfabet-26
+
         letter=chr(in_alfabet)
+
+
+
     output=output+letter
 print output
